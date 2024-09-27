d36805ff02dc83847083ebd389ec342143e74dc5
Zachary Trouy <ztrouy@gmail.com>
2024-09-27 17:21:47 -0500

    Created initial model for the User Address bridge table

---
+++ gondorapi/models/user_address.py
@@ -0,0 +1,10 @@
+from django.db import models
+from .user import User
+from .address import Address
+from .user_address import UserAddress
+from .address_type import AddressType
+
+class UserAddress(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    address = models.ForeignKey(Address, on_delete=models.CASCADE)
+    address_type = models.ForeignKey(AddressType, on_delete=models.CASCADE)
