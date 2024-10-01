fabda129901f7240ab7e523a9978d4f37699503a
Zachary Trouy <34666901+ztrouy@users.noreply.github.com>
2024-10-01 08:02:32 -0500

    Added routing for register endpoint

--- gondorproject/urls.py
+++ gondorproject/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
+from gondorapi.views import UserViewSet
 
 router = routers.DefaultRouter(trailing_slash=False)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path("register", UserViewSet.as_view({"post": "register_account"}), name="register")
 ]
 
