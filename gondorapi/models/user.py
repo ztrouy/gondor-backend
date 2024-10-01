d4c7127558bbf7c3ef3fd9bf2ccb6e667ab29c94
Zachary Trouy <34666901+ztrouy@users.noreply.github.com>
2024-10-01 07:58:26 -0500

    Removed role property, and added custom groups and user_permissions properties

--- gondorapi/models/user.py
+++ gondorapi/models/user.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.models import AbstractUser, Group
+from django.contrib.auth.models import AbstractUser, Group, Permission
 from django.db import models
 import uuid
 
@@ -8,13 +8,15 @@ class User(AbstractUser):
     last_name = models.CharField(max_length=50)
     email = models.EmailField(max_length=100, unique=True)
     date_of_birth = models.DateField()
-    role = models.ForeignKey(Group, on_delete=models.PROTECT)
     is_active = models.BooleanField(default=True)
     created_timestamp = models.DateTimeField(auto_now_add=True)
     updated_timestamp = models.DateTimeField(auto_now=True)
+
+    groups = models.ManyToManyField(Group, related_name="custom_user_groups")
+    user_permissions = models.ManyToManyField(Permission, related_name="custom_user_permissions")
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = ["username"]
 
     def __str__(self):
-        return f"{self.first_name} {self.last_name} ({self.role.name})"
+        return f"{self.first_name} {self.last_name}"
