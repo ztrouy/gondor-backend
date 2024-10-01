b8f2061842df7ad619f6e10b986affbd5f79f654
Zachary Trouy <34666901+ztrouy@users.noreply.github.com>
2024-10-01 08:01:00 -0500

    Updated order of operations to support new authorization scheme

--- seed_database.sh
+++ seed_database.sh
@@ -2,9 +2,10 @@
 
 rm db.sqlite3
 rm -rf ./gondorapi/migrations
-python manage.py migrate
 python manage.py makemigrations gondorapi
 python manage.py migrate gondorapi
-python manage.py loaddata users
-python manage.py loaddata tokens
+python manage.py loaddata groups
+# python manage.py loaddata users
+# python manage.py loaddata tokens
 
+python manage.py migrate
