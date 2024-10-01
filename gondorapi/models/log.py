04e569f08b5700c2310d217cf9048248f3397aaa
Zachary Trouy <34666901+ztrouy@users.noreply.github.com>
2024-10-01 07:57:11 -0500

    Updated viewed_by property to be nullable

--- gondorapi/models/log.py
+++ gondorapi/models/log.py
@@ -5,6 +5,6 @@ import uuid
 
 class Log(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    viewed_by = models.ForeignKey(User, on_delete=models.SET_NULL, related_name="viewed_patient_data")
+    viewed_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name="viewed_patient_data")
     patient_data = models.ForeignKey(PatientData, on_delete=models.CASCADE, related_name="view_logs")
     created_timestamp = models.DateTimeField(auto_now_add=True)
