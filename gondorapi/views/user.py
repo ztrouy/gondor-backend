cd0d37fdde6d3a434fef7a46c8cb85f6e1dd178f
Lincoln-Wilcox <lincolnpepper111@gmail.com>
2024-10-02 13:08:14 -0500

    added view and serializer update method

--- gondorapi/views/user.py
+++ gondorapi/views/user.py
@@ -3,6 +3,25 @@ from rest_framework.decorators import action
 from rest_framework.response import Response
 from gondorapi.models import User
 
+class UserEditSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["email", "first_name", "last_name", "date_of_birth"]
+    
+    def to_internal_value(self, data):
+        data = data.copy()
+        data["first_name"] = data.pop("firstName")
+        data["last_name"] = data.pop("lastName")
+        data["date_of_birth"] = data.pop("dateOfBirth")
+        return super().to_internal_value(data)
+
+    def update(self, instance, validated_data):
+        instance.email = validated_data.get("email", instance.email)
+        instance.first_name = validated_data.get("first_name", instance.first_name)
+        instance.last_name = validated_data.get("last_name", instance.last_name)
+        instance.date_of_birth = validated_data.get("date_of_birth", instance.date_of_birth)
+        instance.save()
+        return instance
 
 class UserSerializer(serializers.ModelSerializer):
     fullName = serializers.SerializerMethodField()
@@ -27,3 +46,6 @@ class UserViewSet(viewsets.ViewSet):
     def get_my_details(self, request):
         serializer = UserSerializer(request.user)
         return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    @action(detail=False, method=["put"], url_path="edit-name")
+    def update(self, request):
