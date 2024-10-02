bbf2cc009516e21f5aff3144bf1c1e42ee32c0bf
Zachary Trouy <34666901+ztrouy@users.noreply.github.com>
2024-10-02 08:30:54 -0500

    Added constraint to prevent duplicate entries, and updated id to UUID

--- gondorapi/models/patient_clinician.py
+++ gondorapi/models/patient_clinician.py
@@ -1,6 +1,13 @@
 from django.db import models
 from .user import User
+import uuid
 
 class PatientClinician(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     patient = models.ForeignKey(User, on_delete=models.CASCADE, related_name="patients")
     clinician = models.ForeignKey(User, on_delete=models.CASCADE, related_name="clinicians")
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=["patient", "clinician"], name="unique_patient_clinician")
+        ]
