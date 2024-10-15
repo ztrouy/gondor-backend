4d2f1b2819ae6d2b990e455b49ffb18e1f7155eb
Lincoln-Wilcox <lincolnpepper111@gmail.com>
2024-10-15 10:51:15 -0500

    added clinician serializer

--- gondorapi/views/appointment.py
+++ gondorapi/views/appointment.py
@@ -1,39 +1,56 @@
 from rest_framework import viewsets, status, serializers, permissions
-from gondorapi.models import Appointment
+from gondorapi.models import Appointment, User
 import datetime
 from rest_framework.decorators import action
 from rest_framework.response import Response
 
+class PatientAppointmentClinicianSerializer(serializers.ModelSerializer):
+    fullName = serializers.SerializerMethodField()
+
+    class Meta:
+        model = User
+        fields = ["id", "first_name", "last_name", "fullName"]
+    
+    def to_representation(self, instance):
+        rep = super().to_representation(instance)
+        rep["firstName"] = rep.pop("first_name")
+        rep["lastName"] = rep.pop("last_name")
+        return rep
+
+    def get_fullName(self, obj):
+        return f"{obj.clinician.first_name} {obj.clinician.last_name}"
+
 class PatientAppointmentSerializer(serializers.ModelSerializer):
-    clinicianName = serializers.SerializerMethodField()
-    isApproved = serializers.SerializerMethodField()
+    clinician = PatientAppointmentClinicianSerializer(read_only = True)
     isCompleted = serializers.SerializerMethodField()
 
     class Meta:
         model = Appointment
-        fields = ["id", "scheduled_timestamp", "clinician", "isPending", "isApproved", "isCompleted"]
+        fields = ["id", "scheduled_timestamp", "clinician", "isPending", "is_approved", "isCompleted"]
 
     def to_representation(self, instance):
         rep = super().to_representation(instance)
-        rep["scheduledTimestamp"] = rep.pop("scheduled_timestamp")
+        rep["scheduledDate"] = rep.pop("scheduled_timestamp")
+        rep["isApproved"] = rep.pop("is_approved")
         return rep
 
     # def get_clinicianName(self, obj):
     #     return f"{obj.clinician.first_name} {obj.clinician.last_name}"
-    
-    def get_isApproved(self, obj):
-        return None if obj.approver == None and not obj.is_approved else obj.is_approved
 
-    def get_isCompleted(self, obj):
-        return obj.is_approved and obj.is_checked_in and (datetime.datetime.now() - obj.scheduledTimestamp).minutes > 30
+    # def get_isCompleted(self, obj):
+    #     return obj.is_approved and obj.is_checked_in and (datetime.datetime.now() - obj.scheduledTimestamp).minutes > 30
+
+
 
 class AppointmentViewSet(viewsets.ViewSet):
     @action(detail=False, methods=["get"], url_path="my")
     def get_patient_appointments(self, request):
-        todays_date = datetime.date.today() 
-        after_date = request.GET.get("afterDate") if request.GET.get("afterDate") != None else "0001-01-01"
-        before_date = request.GET.get("beforeDate") if request.GET.get("beforeDate") != None else datetime.date(todays_date.year + 1, todays_date.month, todays_date.day).strftime("%Y-%m-%d")
-        clinician_filter = request.GET.get("clinicianName") if request.GET.get("clinicianName") != None else ""
-        appointments = Appointment.objects.filter(patient=request.user, scheduled_timestamp__range=[after_date, before_date], clinician__last_name__icontains=clinician_filter)
-        serializer = PatientAppointmentSerializer(appointments, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+        # todays_date = datetime.date.today() 
+        # after_date = request.GET.get("after") if request.GET.get("after") != None else "0001-01-01"
+        # before_date = request.GET.get("before") if request.GET.get("before") != None else datetime.date(todays_date.year + 1, todays_date.month, todays_date.day).strftime("%Y-%m-%d")
+        # clinician_filter = request.GET.get("clinicianId") if request.GET.get("clinicianName") != None else ""
+        # appointments = Appointment.objects.filter(patient=request.user, scheduled_timestamp__range=[after_date, before_date], clinician__last_name__icontains=clinician_filter)
+        # serializer = PatientAppointmentSerializer(appointments, many=True)
+        #return Response(serializer.data, status=status.HTTP_200_OK)
+
+        return
