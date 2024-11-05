2e95e15ba55ea89c54afb8dc4dbb338780d32d33
Chad Cote <chadcote58@gmail.com>
2024-11-05 18:42:07 -0500

    Created serializers to handle viewing a patients data and creating a log

--- gondorapi/views/records.py
+++ gondorapi/views/records.py
@@ -1,10 +1,18 @@
-from gondorapi.models import PatientData, User
-from .user import ClinicianSerializer 
+from gondorapi.models import PatientData, User, Log
+from .users import ClinicianSerializer 
 from rest_framework import viewsets, status, serializers
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from django.db.models import Q
 from django.contrib.auth.models import Group
+
+class PatientSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["id", "first_name", "last_name", "fullName"]
+
+    def get_fullName(self,obj):
+        return f"{obj.first_name} {obj.last_name}"
 
 
 class PatientDataSerializer(serializers.ModelSerializer):
@@ -17,7 +25,42 @@ class PatientDataSerializer(serializers.ModelSerializer):
     def get_clinician(self,obj):
         serializer = ClinicianSerializer(obj.created_by)
         return serializer.data
+    
+class SpecificPatientDataSerializer(serializers.ModelSerializer):
+    clinician = serializers.SerializerMethodField()
+    updating_clinician = serializers.SerializerMethodField()
+    patient = serializers.SerializerMethodField()
 
+    class Meta:
+        model = PatientData
+        fields = ["id", "patient", "appointment", "clinician", "updating_clinician", "created_timestamp", "patient_diastolic", "patient_systolic", "is_notes_shared", "clinician_notes"]
+
+    def to_representation(self, instance):
+        rep = super().to_representation(instance)
+        rep["updatedClinician"] = rep.pop("updating_clinician")
+        rep["createdTimestamp"] = rep.pop("created_timestamp")
+        rep["patientDiastolic"] = rep.pop("patient_diastolic")
+        rep["patientSystolic"] = rep.pop("patient_systolic")
+        rep["isNotesShared"] = rep.pop("is_notes_shared")
+        rep["clinicianNotes"] = rep.pop("clinician_notes")
+        return rep
+    
+    def get_patient(self,obj):
+        serializer = PatientSerializer(obj.patient)
+        return serializer.data
+    
+    def get_clinician(self,obj):
+        serializer = ClinicianSerializer(obj.created_by)
+        return serializer.data
+    
+    def get_updating_clinician(self,obj):
+        serializer = ClinicianSerializer(obj.updated_by)
+        return serializer.data
+
+class CreateLogSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Log
+        fields = ["viewed_by", "patient_data", "created_timestamp"]
 
 class RecordViewSet(viewsets.ViewSet):
     @action(detail=False, methods=["get"], url_path="me")
@@ -41,3 +84,5 @@ class RecordViewSet(viewsets.ViewSet):
             return Response(serializer.data, status=status.HTTP_200_OK)
         
         return Response({"error": "Unauthorized access. Only patients can access their records"}, status=status.HTTP_400_BAD_REQUEST)
+    
+    
