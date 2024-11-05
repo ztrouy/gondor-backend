bf2ab4f11f7c789e5b5c955c645f8895d979327d
Chad Cote <chadcote58@gmail.com>
2024-11-05 13:39:15 -0500

    Added logic to check if the user accessing the records is a patient otherwise return an error with description

--- gondorapi/views/records.py
+++ gondorapi/views/records.py
@@ -4,6 +4,7 @@ from rest_framework import viewsets, status, serializers
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from django.db.models import Q
+from django.contrib.auth.models import Group
 
 
 class PatientDataSerializer(serializers.ModelSerializer):
@@ -21,17 +22,22 @@ class PatientDataSerializer(serializers.ModelSerializer):
 class RecordViewSet(viewsets.ViewSet):
     @action(detail=False, methods=["get"], url_path="me")
     def get_my_records(self, request):
-        filters = Q(patient=request.user)
+        user = request.user
 
-        before_date = request.GET.get("before")
-        after_date = request.GET.get("after")
+        if user.groups.filter(name="Patient").exists():
+            filters = Q(patient=request.user)
 
-        if before_date != None:
-            filters &= Q(created_timestamp__lt = before_date)
+            before_date = request.GET.get("before")
+            after_date = request.GET.get("after")
 
-        if after_date != None:
-            filters &= Q(created_timestamp__gt=after_date)
+            if before_date != None:
+                filters &= Q(created_timestamp__lt = before_date)
 
-        records = PatientData.objects.filter(filters)
-        serializer = PatientDataSerializer(records, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+            if after_date != None:
+                filters &= Q(created_timestamp__gt=after_date)
+
+            records = PatientData.objects.filter(filters)
+            serializer = PatientDataSerializer(records, many=True)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        
+        return Response({"error": "Unauthorized access. Only patients can access their records"})
