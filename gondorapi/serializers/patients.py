08474bf0d1f07844983008d6097d833592cb637f
Zachary Trouy <34666901+ztrouy@users.noreply.github.com>
2024-11-13 11:51:05 -0600

    Updated how lastName and Appointments are handled

--- gondorapi/serializers/patients.py
+++ gondorapi/serializers/patients.py
@@ -1,24 +1,25 @@
+from rest_framework import serializers
 from gondorapi.models import User
 from gondorapi.serializers import EmbeddedSerializers
-from rest_framework import serializers
 
 
 class PatientSerializers:
     class PatientSerializer(serializers.ModelSerializer):
         class Meta:
             model = User
-            fields = ["id", "first_name", "last_name", "fullName", "is_active"]
+            fields = ["id", "first_name", "last_name", "full_name", "is_active"]
 
         def to_representation(self, instance):
             rep = super().to_representation(instance)
             rep["firstName"] = rep.pop("first_name")
             rep["lastName"] = rep.pop("last_name")
+            rep["fullName"] = rep.pop("full_name")
             rep["isActive"] = rep.pop("is_active")
             return rep
         
 
     class PatientWithAppointmentSerializer(serializers.ModelSerializer):
-        appointment = serializers.DateTimeField()
+        appointment = serializers.SerializerMethodField()
         
         class Meta:
             model = User
@@ -30,6 +31,10 @@ class PatientSerializers:
             rep["lastName"] = rep.pop("last_name")
             rep["fullName"] = rep.pop("full_name")
             return rep
+
+        def get_appointment(self, obj):
+            serializer = EmbeddedSerializers.EmbeddedAppointmentSimpleSerializer(obj.next_appointment, read_only=True)
+            return serializer.data
         
 
     class PatientWithDateOfBirthSerializer(serializers.ModelSerializer):
@@ -47,12 +52,12 @@ class PatientSerializers:
         
 
     class PatientWithAppointmentAndDateOfBirthSerializer(serializers.ModelSerializer):
-        appointment = EmbeddedSerializers.EmbeddedAppointmentSimpleSerializer(read_only=True)
+        appointment = serializers.SerializerMethodField()
 
         class Meta:
             model = User
             fields = ["id", "first_name", "last_name", "full_name", "date_of_birth", "appointment"]
-        
+
         def to_representation(self, instance):
             rep = super().to_representation(instance)
             rep["firstName"] = rep.pop("first_name")
@@ -60,3 +65,9 @@ class PatientSerializers:
             rep["fullName"] = rep.pop("full_name")
             rep["dateOfBirth"] = rep.pop("date_of_birth")
             return rep
+
+        def get_appointment(self, obj):
+            if obj.appointment:
+                serializer = EmbeddedSerializers.EmbeddedAppointmentSimpleSerializer(obj.appointment, read_only=True)
+                return serializer.data
+            return None
