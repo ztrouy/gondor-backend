3010b7d85901dffec7420c923ce0db88c18b3cc6
Chad Cote <chadcote58@gmail.com>
2024-11-20 13:12:16 -0500

    Removed user property, removed import, changed constraint to use address instead of user

--- gondorapi/models/user_address.py
+++ gondorapi/models/user_address.py
@@ -1,12 +1,11 @@
 from django.db import models
-from .user import User
 from .address import Address
 from .address_type import AddressType
 import uuid
 
 class UserAddress(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    address = models.ForeignKey(Address, on_delete=models.CASCADE)
+    address = models.ForeignKey(Address, on_delete=models.CASCADE, related_name="active_addresses")
     address_type = models.ForeignKey(AddressType, on_delete=models.CASCADE)
 
     class Meta:
